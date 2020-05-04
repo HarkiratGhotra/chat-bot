5d113989ffb1070281d34a563e8328996408af34
Harkirat Ghotra <harkiratghotra@HK.local>
2020-05-04 18:21:59 -0400

    fixing the build issue and also fixed the angularJs unknow provider issue. I have added mode development to build the app

--- platforms/android/app/build/intermediates/merged_assets/debug/mergeDebugAssets/out/www/directive/chatBotPage/chatBotPage.js
+++ platforms/android/app/build/intermediates/merged_assets/debug/mergeDebugAssets/out/www/directive/chatBotPage/chatBotPage.js
@@ -3,90 +3,86 @@ import templateStr from './chat-bot-page.html';
 export default [() => {
     return {
         template:templateStr,
-        constroller:DashboardPanelController
-        // controller: ['$scope','$http','$timeout', function ($scope,$http,$timeout) {
-		// 	$scope.form = {
-		// 		input:''
-        //     }
-        //     $scope.hideOptions = false;
-        //     let chatData;
-        //     $scope.chatList = [];
-		// 	$http.get('http://localhost:3000/api/conversation').then((result) => {
-        //         $scope.initialChat = result.data.units[0];
-        //         chatData = result.data;
-        //      });
+        controller: ['$scope','$http','$timeout', ($scope,$http,$timeout) => {
+			$scope.form = {
+				input:''
+            }
+            $scope.hideOptions = false;
+            let chatData;
+            $scope.chatList = [];
+			$http.get('http://localhost:3000/api/conversation').then((result) => {
+                $scope.initialChat = result.data.units[0];
+                chatData = result.data;
+             });
 
-		// 	$scope.updateChat = (index) => {
-        //         $scope.selectedOption = $scope.initialChat.options[index];
-        //         $scope.hideOptions = true;
-        //         $scope.hasUserSelectedOption = true;
-        //     };
-        //     $scope.getNextResponse = (id) => {
-        //         $http.get("http://localhost:3000/api/conversation/" + id).then((data) => {
-        //             $scope.updatedResult = data.data;
-        //             $scope.chatList.push($scope.updatedResult.responses[0]);
-        //         });
-        //     };
+			$scope.updateChat = (index) => {
+                $scope.selectedOption = $scope.initialChat.options[index];
+                $scope.hideOptions = true;
+                $scope.hasUserSelectedOption = true;
+            };
+            $scope.getNextResponse = (id) => {
+                $http.get("http://localhost:3000/api/conversation/" + id).then((data) => {
+                    $scope.updatedResult = data.data;
+                    $scope.chatList.push($scope.updatedResult.responses[0]);
+                });
+            };
             
-        //     $scope.$watch('hideOptions',(nv, ov) => {
-        //         if(nv != ov) {
-        //             if($scope.selectedOption && $scope.selectedOption.nextUnit){
-        //                 $scope.getNextResponse($scope.selectedOption.nextUnit);
-        //             }
-        //         }
-        //     });
+            $scope.$watch('hideOptions',(nv, ov) => {
+                if(nv != ov) {
+                    if($scope.selectedOption && $scope.selectedOption.nextUnit){
+                        $scope.getNextResponse($scope.selectedOption.nextUnit);
+                    }
+                }
+            });
 
-        //     $scope.$watch('updatedResult',(nv,ov) => {
-        //         if(nv != ov && !nv.component) {
-        //             $scope.getNextResponse(nv.nextUnit);
-        //             $timeout(()=>{
-        //                 $scope.annimateResponses = true
-        //             },500);
-        //         };
-        //     });
+            $scope.$watch('updatedResult',(nv,ov) => {
+                if(nv != ov && !nv.component) {
+                    $scope.getNextResponse(nv.nextUnit);
+                    $timeout(()=>{
+                        $scope.annimateResponses = true
+                    },500);
+                };
+            });
 
-		// 	$scope.submitReply = (input) => {
-		// 		if(input.length <= 20 ) {
-		// 			$scope.expression = 'input.length <= 20';
-		// 		}else {
-		// 			$scope.expression = 'input.length > 20';
-        //         }
+			$scope.submitReply = (input) => {
+				if(input.length <= 20 ) {
+					$scope.expression = 'input.length <= 20';
+				}else {
+					$scope.expression = 'input.length > 20';
+                }
 
-        //         $http({
-        //             url: 'http://localhost:3000/api/conversation',
-        //             method: "POST",
-        //             headers: {
-        //                 'content-type': 'application/json'
-        //             },
-        //             data:{ 'response' : input }
-        //         }).then((result) =>{
-        //             chatData = result.data;
-        //             if($scope.updatedResult && $scope.updatedResult.component) {
-        //                 $scope.lastResponse = chatData.units.find(item => {
-        //                     if(item.id === $scope.updatedResult.component.nextUnit){
-        //                         item.condition.find(item => {
-        //                             if(item.expression === $scope.expression) {
-        //                                 $scope.nextUnit = item.nextUnit;
-        //                             }
-        //                         })
-        //                     }
-        //                     if($scope.nextUnit && item.id === $scope.nextUnit) {
-        //                         return item;
-        //                     }
-        //                 })
-        //             };
+                $http({
+                    url: 'http://localhost:3000/api/conversation',
+                    method: "POST",
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    data:{ 'response' : input }
+                }).then((result) =>{
+                    chatData = result.data;
+                    if($scope.updatedResult && $scope.updatedResult.component) {
+                        $scope.lastResponse = chatData.units.find(item => {
+                            if(item.id === $scope.updatedResult.component.nextUnit){
+                                item.condition.find(item => {
+                                    if(item.expression === $scope.expression) {
+                                        $scope.nextUnit = item.nextUnit;
+                                    }
+                                })
+                            }
+                            if($scope.nextUnit && item.id === $scope.nextUnit) {
+                                return item;
+                            }
+                        })
+                    };
 
-        //             if($scope.lastResponse && !$scope.lastResponse.nextUnit) {
-        //                 $scope.repliedMessage = chatData.units.slice(-1)[0].reply;
-        //             }else{
-        //                 $scope.repliedMessage = '';
-        //             }
-        //             $scope.form={}
-        //         });
-		// 	}
-        // }]
+                    if($scope.lastResponse && !$scope.lastResponse.nextUnit) {
+                        $scope.repliedMessage = chatData.units.slice(-1)[0].reply;
+                    }else{
+                        $scope.repliedMessage = '';
+                    }
+                    $scope.form={}
+                });
+			}
+        }]
     }
-    DashboardPanelController.$inject = ['logger'];
-function DashboardPanelController(logger) {
-}
 }];
